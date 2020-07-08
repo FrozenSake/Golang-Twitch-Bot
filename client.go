bf74202e62336b8b0c0cf599e538f85ad716cba0
FrozenSake <FrozenSake@gmail.com>
2020-07-07 20:17:07 -0400

    Formatting

--- client.go
+++ client.go
@@ -6,22 +6,25 @@ import (
 )
 
 const (
+	// ircTwitch for Twitch's IRC constants.
 	ircTwitchTLS = "irc://irc.chat.twitch.tv:6697"
 	ircTwitch    = "irc://irc.chat.twitch.tv:6667"
 	ircInvalid   = "421"
 
+	// websocketTwitch for Twitch's websocket constants.
 	webSocketTwitchTLS = "wss://irc-ws.chat.twitch.tv:443"
 	webSocketTwitch    = "ws://irc-ws.chat.twitch.tv:80"
 
+	// pingmMessage for Twitch's PING message as sourced from https://dev.twitch.tv/docs/irc/guide .
 	pingMessage = "PING :tmi.twitch.tv"
 
-	// https://dev.twitch.tv/docs/irc/commands -- CAP REQ :twitch.tv/commands
+	// CommandsCapability for Twitch's Commands: https://dev.twitch.tv/docs/irc/commands -- CAP REQ :twitch.tv/commands
 	CommandsCapability = "twitch.tv/commands"
 
-	// https://dev.twitch.tv/docs/irc/membership -- CAP REQ :twitch.tv/membership
+	// MembershipCapability for Twitch's Memberships: https://dev.twitch.tv/docs/irc/membership -- CAP REQ :twitch.tv/membership
 	MembershipCapability = "twitch.tv/membership"
 
-	// https://dev.twitch.tv/docs/irc/tags -- CAP REQ :twitch.tv/tags
+	// TagsCapability for Twitch's Tags: https://dev.twitch.tv/docs/irc/tags -- CAP REQ :twitch.tv/tags
 	TagsCapability = "twitch.tv/membership"
 )
 
@@ -33,18 +36,18 @@ func NewAnonymousIRCClient() {
 	return NewIRCClient("justinfan1234321", "oauth:99999")
 }
 
-func Join {
+func Join() {
 
 }
 
-func Part {
+func Part() {
 
 }
 
-func PrivMsg {
+func PrivMsg() {
 
 }
 
-func NewWebSocketClient (username, oauth string) {
+func NewWebSocketClient(username, oauth string) {
 
 }
