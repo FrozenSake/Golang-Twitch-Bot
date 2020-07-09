72eaa037a3aad073b3bab846436d240df07349fc
FrozenSake <FrozenSake@gmail.com>
2020-07-08 22:14:01 -0400

    Built command database: DBPrepare, DBCommandSelect, DBCommandInsert

--- bot.go
+++ bot.go
@@ -2,24 +2,36 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/gempir/go-twitch-irc/v2"
 	"github.com/joho/godotenv"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const (
 	oauthForm = "oauth:"
+	// First group is command, second group is payload
+	commandRegex = "^!(\\S+) ?(.*)"
 )
 
 var (
-	username string
-	oauth    string
-	targets  []string
+	username    string
+	oauth       string
+	targets     []string
+	commandList [][2]string
+	channels    map[string]broadcaster
 )
+
+type broadcaster struct {
+	name     string
+	database *sql.DB
+}
 
 func goDotEnvVariable(key string) string {
 
@@ -38,6 +50,78 @@ func OauthCheck() {
 	}
 }
 
+func DoCommand(command string, ch broadcaster, re *regexp.Regexp) string {
+	submatch := re.FindStringSubmatch(command)
+	trigger := strings.ToLower(submatch[1])
+	options := submatch[2]
+	var result string
+
+	if trigger == "addcommand" {
+		result = DBCommandInsert(trigger, options, ch.name)
+	} else {
+		result = DBCommandSelect(trigger, ch.name)
+	}
+
+	return result
+}
+
+func DBPrepare(channelName string) *sql.DB {
+	dbname := "./" + channelName + ".db"
+
+	database, err := sql.Open("sqlite3", dbname)
+	if err != nil {
+		panic(err)
+	}
+
+	statement, err := database.Prepare("CREATE TABLE IF NOT EXISTS commands (id INTEGER PRIMARY KEY, trigger TEXT UNIQUE, payload TEXT)")
+	if err != nil {
+		panic(err)
+	}
+	statement.Exec()
+
+	return database
+}
+
+func DBCommandSelect(trigger, channelName string) string {
+	dbname := "./" + channelName + ".db"
+	selectStatement := "SELECT payload FROM commands WHERE trigger = '" + trigger + "';"
+
+	database, err := sql.Open("sqlite3", dbname)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Before DB Query with statement: ", selectStatement)
+	rows, err := database.Query(selectStatement)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("After DB Query")
+
+	var payload string
+	for rows.Next() {
+		rows.Scan(&payload)
+	}
+
+	return payload
+}
+
+func DBCommandInsert(trigger, payload, channelName string) string {
+	dbname := "./" + channelName + ".db"
+	database, err := sql.Open("sqlite3", dbname)
+	if err != nil {
+		panic(err)
+	}
+
+	statement, err := database.Prepare("INSERT INTO commands (trigger, payload) VALUES (?, ?)")
+	if err != nil {
+		panic(err)
+	}
+	statement.Exec(trigger, payload)
+
+	return "Command " + trigger + " added succesfully."
+}
+
 func main() {
 
 	username = goDotEnvVariable("username")
@@ -48,24 +132,34 @@ func main() {
 	fmt.Println(targets)
 
 	OauthCheck()
+	channels = make(map[string]broadcaster)
+
+	// Define a regex object
+	re := regexp.MustCompile(commandRegex)
 
 	client := twitch.NewClient(username, oauth)
 
+	for _, channelName := range targets {
+		channelName = strings.ToLower(channelName)
+		client.Join(channelName)
+		fmt.Printf("##USERLIST FOR %v##\n", channelName)
+		userlist, _ := client.Userlist(channelName)
+		fmt.Printf("Users: %v\n", userlist)
+		DB := DBPrepare(channelName)
+		bc := broadcaster{name: channelName, database: DB}
+		channels[channelName] = bc
+	}
+
 	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
 		fmt.Printf("%v - %v: %v\n", message.Channel, message.User.DisplayName, message.Message)
-		if message.Message[:1] == "!" {
-			fmt.Println("##Command detected!##")
+		if re.MatchString(message.Message) {
+			fmt.Println("##Possible Command detected!##")
 			target := message.Channel
-			client.Say(target, "Hi, this is Hikthur testing.")
+			command := DoCommand(message.Message, channels[target], re)
+			fmt.Printf("Command is: %v", command)
+			client.Say(target, command)
 		}
 	})
-
-	for _, channel := range targets {
-		client.Join(channel)
-		fmt.Printf("##USERLIST FOR %v##\n", channel)
-		userlist, _ := client.Userlist(channel)
-		fmt.Printf("Users: %v\n", userlist)
-	}
 
 	err := client.Connect()
 	if err != nil {
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/gempir/go-twitch-irc/v2 v2.4.0
 	github.com/joho/godotenv v1.3.0
+	github.com/mattn/go-sqlite3 v1.14.0
 )
